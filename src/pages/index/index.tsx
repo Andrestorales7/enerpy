ff044a7ec978d41f164daed54b73e1aaa36e792e
francobg7 <francobg7@gmail.com>
2025-10-09 05:46:30 -0300

    last part index clean

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,7 +4,6 @@ import WhatWeDoSection from "@/components/WhatWeDoSection";
 import TechnologySection from "@/components/TechnologySection";
 import EnvironmentalImpactSection from "@/components/EnvironmentalImpactSection";
 import ProductsSection from "@/components/ProductsSection";
-import ContributeSection from "@/components/ContributeSection";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import Navbar from "@/components/Navbar";
@@ -19,7 +18,6 @@ const Index = () => {
       <TechnologySection />
       <EnvironmentalImpactSection />
       <ProductsSection />
-      <ContributeSection />
       <Footer />
       <ScrollToTop />
     </div>
